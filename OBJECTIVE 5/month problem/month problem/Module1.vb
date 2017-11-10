22070a791f154b0fee2d559d8f92416dc345970a
logan <logan99miller@gmail.com>
2017-11-10 16:32:10 +0000

    Objective 5

---
+++ OBJECTIVE 5/month problem/month problem/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim year, month, days As Integer
+        Dim monthName() As String = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
+        Dim monthLength() As String = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+        Console.WriteLine("Please enter a year:")
+        year = Console.ReadLine()
+        Console.WriteLine("Please enter a month (e.g. 1, 5 or 12:")
+        month = Console.ReadLine()
+        If year Mod 4 = 0 Then
+            monthLength(1) = 29
+        End If
+        days = monthLength(month - 1)
+        Console.WriteLine(monthName(month - 1) & " has " & days & " days in " & year)
+        Console.ReadLine()
+        Main()
+    End Sub
+
+End Module
---
+++ OBJECTIVE 5/rpg dice problem/rpg dice problem/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim sides, randomInteger As Integer
+        Dim randomDecimal As Decimal
+
+        Console.WriteLine("How many sides does your dice have?")
+        sides = Console.ReadLine()
+
+        Randomize() 'generates random seed
+        randomDecimal = Rnd() * (sides - 1) 'random decimal multiplied by amount of sides -1 ( as 1 is added later)
+        randomInteger = CInt(randomDecimal + 1) 'converted to decimal and then add 1 to prevent 0s
+        Console.WriteLine("You rolled a " & randomInteger)
+        Console.ReadLine()
+    End Sub
+
+End Module
