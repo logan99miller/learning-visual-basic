84ff50fb91169e8f02e6098fa0446be3ef4f0180
logan <logan99miller@gmail.com>
2017-11-10 16:30:15 +0000

    Objective 5

---
+++ OBJECTIVE 5/cash machine/cash machine/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim balance, withdraw, remainder, smallestNote As Integer
+        balance = 231
+        smallestNote = 10
+        Console.WriteLine("Your balance is: " & balance)
+        Console.WriteLine("How much do you wish to withdraw?")
+        withdraw = Console.ReadLine()
+        remainder = withdraw Mod smallestNote
+        If (remainder = 0) And (withdraw <= 231) Then
+            balance = balance - withdraw
+            Console.WriteLine("Your balance is: " & balance)
+        Else
+            Console.WriteLine("You cannot withdraw this amount")
+        End If
+        Console.ReadLine()
+        Main()
+    End Sub
+
+End Module
---
+++ OBJECTIVE 5/dice game/dice game/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim dice() As Integer
+        Randomize()
+        For i = 0 To 2
+            dice(i) = CInt(Rnd() * 6) - 1
+            Console.WriteLine("Dice " & i & " rolled a " & dice(i))
+        Next
+        Console.ReadLine()
+
+    End Sub
+
+End Module
