ee76b0b3ebdb0e9c7dfe04b0c846af40bb1dee37
logan <logan99miller@gmail.com>
2017-11-10 16:28:11 +0000

    Objective 11

---
+++ OBJECTIVE 11/word extractor/word extractor/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim sentence, word, replacementStart As String
+        sentence = "quick brown fox jumps over the lazy dog"
+        While True
+            Console.WriteLine("Please enter a word to be extaracted from the sentence: " & sentence)
+            word = Console.ReadLine()
+            If sentence.Contains(word) Then
+                replacementStart = InStr(sentence, word) 'which character in the sentence is the first character of the word
+
+                'Keeps everything left of the character before the word: -1 to ensure it does not keep first letter of word
+                ' & everything right of the word: length of sentence - where word starts - rest of word
+                sentence = Left(sentence, replacementStart - 1) & Right(sentence, Len(sentence) - replacementStart - Len(word))
+            Else
+                Console.WriteLine("The sentence does not contain this word :(. Try again")
+            End If
+        End While
+    End Sub
+
+End Module
---
+++ OBJECTIVE 11/word extractor/word extractor/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("word extractor")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("word extractor")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7cc0bdd0-1f1b-41d1-9ef3-147e8e016dc1")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
