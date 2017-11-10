1e4a8b082bb63a6ddc16acd6223ed4140d049089
logan <logan99miller@gmail.com>
2017-11-10 16:33:08 +0000

    Objective 5

---
+++ OBJECTIVE 5/objective 5 learning/objective 5 learning/Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+
+    Sub Main()
+        Randomize()
+        For i = 0 To 10
+            Console.WriteLine(CInt(Rnd() * 5) + 1)
+        Next
+        Console.ReadLine()
+    End Sub
+
+End Module
