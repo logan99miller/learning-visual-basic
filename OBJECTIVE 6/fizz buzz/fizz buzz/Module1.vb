bf1d00a0fc6f0089f91e4475e59888eb6eb72d79
logan <logan99miller@gmail.com>
2017-11-10 16:21:20 +0000

    Objective 6

---
+++ OBJECTIVE 6/fizz buzz/fizz buzz/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        For repeats = 1 To 100
+            If (repeats Mod 3 = 0) And (repeats Mod 5 = 0) Then
+                Console.WriteLine("FizzBuzz")
+            ElseIf repeats Mod 3 = 0 Then
+                Console.WriteLine("Fizz")
+            ElseIf repeats Mod 5 = 0 Then
+                Console.WriteLine("Buzz")
+            Else
+                Console.WriteLine(repeats)
+            End If
+        Next
+        Console.ReadLine()
+        Main()
+    End Sub
+
+End Module
---
+++ OBJECTIVE 6/maths test/maths test/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim integer1, integer2, answer, correct As Integer
+        Randomize()
+        For repeats = 1 To 10
+            integer1 = CInt((Rnd() * 9) + 1)
+            integer2 = CInt((Rnd() * 9) + 1)
+            Console.WriteLine("What is " & integer1 & " + " & integer2 & "?")
+            answer = Console.ReadLine()
+            If integer1 + integer2 = answer Then
+                Console.WriteLine("You are correct :)")
+                correct = correct + 1
+            Else
+                Console.WriteLine("You are wrong :(")
+            End If
+        Next
+        Console.WriteLine("You got " & correct & " questions right")
+        Console.ReadLine()
+        Main()
+    End Sub
+
+End Module
