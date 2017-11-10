8267300833f830eb737d7d7263085afba0d6d8ae
logan <logan99miller@gmail.com>
2017-11-10 16:19:01 +0000

    Objective 5

---
+++ OBJECTIVE 4/Portfolio grade/Portfolio grade/Module1.vb
@@ -0,0 +1,46 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim marks As Integer
+        Dim boundary2, boundary3, boundary4, boundary5, boundary6, boundary7, boundary8, boundary9 As Integer
+        boundary2 = 4
+        boundary3 = 13
+        boundary4 = 22
+        boundary5 = 31
+        boundary6 = 41
+        boundary7 = 54
+        boundary8 = 67
+        boundary9 = 80
+        Dim grade As String
+        Dim sections() As String = {"analysis", "design", "implementation", "evaluation"} 'contains each section
+        For repeats = 0 To sections.Length() - 1 'asks for each section
+            Console.WriteLine(vbCrLf & "Enter your marks for " & sections(repeats) & ":") 'ask for relevant section
+            marks = marks + Console.ReadLine() 'appends to total marks
+        Next
+        Console.WriteLine(vbCrLf & "Total marks = " & marks)
+        Select Case marks
+            Case Is < boundary2
+                grade = "1"
+            Case Is < boundary3
+                grade = "2"
+            Case Is < boundary4
+                grade = "3"
+            Case Is < boundary5
+                grade = "4"
+            Case Is < boundary6
+                grade = "5"
+            Case Is < boundary7
+                grade = "6"
+            Case Is < boundary8
+                grade = "7"
+            Case Is < boundary9
+                grade = "8"
+            Case Else
+                grade = "9"
+        End Select
+        Console.WriteLine("You achieved a grade: " & grade)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ OBJECTIVE 4/Portfolio grade/Portfolio grade/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Portfolio grade")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Portfolio grade")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("93263f28-0320-48b2-a7cf-76734b579db3")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
